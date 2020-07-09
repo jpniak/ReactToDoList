a0c9e607e551d1a4c09917fcd0f8d9f392c8df51
Jakub Pniak <jpniak@o2.pl>
2020-07-09 17:52:01 +0200

    Add components: List, Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+const Hero = () => (
+  <header className={styles.component}>    
+    <h2 className={styles.title}>Things to do</h2>
+    <img className={styles.image} src="./src/images/hero.png" />
+  </header>
+);
+  
+
+
+export default Hero;
