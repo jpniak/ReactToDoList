7144fddb2f9f8a49c8bd6f13223f9dc120771e3a
Jakub Pniak <jpniak@o2.pl>
2020-07-16 15:24:37 +0200

    Add redux to the project and start migration

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,18 +2,15 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+//import Column from '../Column/Column.js';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 
 
 
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }
 
   static propTypes = {
     title: PropTypes.node.isRequired,
@@ -26,31 +23,19 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   }
 
-  addColumn(title) {
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length - 1].key + 1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
-
+  
 
 
   render() {
+    const {title, image, description} = this.props;
+
     return (
-      <section id="list" className={styles.component}>
-        <Hero titleText={this.props.title} imageSourceText={this.props.image} />
+      <section className={styles.component}>
+        <Hero titleText={title} imageSourceText={image} />
         <div className={styles.description}>
-          {ReactHtmlParser(this.props.description)}
+          {ReactHtmlParser(description)}
         </div>
+        {/*
         <div className={styles.columns}>
           {this.state.columns.map(({ key, ...columnProps }) => (
             <Column key={key} {...columnProps} />
@@ -61,6 +46,7 @@ class List extends React.Component {
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
 
         </div>
+        */}
 
       </section>
     );
